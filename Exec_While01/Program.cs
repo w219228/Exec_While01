763788babe24479276b790450eab7f3d3f576cb2
ispan <ispan@DESKTOP-PAKSETB>
2022-10-24 17:38:43 +0800

    c

---
+++ Exec_While01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_While01
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int num1 = 0;
+			int num2 = 105;
+			int sum = 0;
+			while(num1<num2)
+			{
+				num1++;
+				sum += num1;
+				if (sum > num2)
+				{
+					break;
+				}
+				
+			}
+			Console.WriteLine(num1);
+		}
+	}
+}
